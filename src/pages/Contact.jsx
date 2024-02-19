560a6fd637ba524cafbd6e9a1c2ade8a7100c7a2
AoifeEdX <ayohwife@gmail.com>
2024-02-19 19:58:27 +0000

    Set up Contact template page (importing header, main content, footer)

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header'
+import ContactPage from '../components/ContactPage'
+import Footer from '../components/Footer'
+
+function Contact() {
+  return (
+    <>
+      <Header />
+      <ContactPage />
+			<Footer />
+    </>
+  )
+}
+
+export default Contact
