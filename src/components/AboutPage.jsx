008043eca75b7fa4b5a5e0276ae31045c193f7a8
AoifeEdX <ayohwife@gmail.com>
2024-02-19 22:13:23 +0000

    Added body text to About page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -21,9 +21,12 @@ function AboutPage() {
 						<h2>About Me</h2>
 						{/* Horizontal line */}
 						<hr></hr>
-						<p className="my-5 fs-5">
-							<span>Lorem ipsum</span> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						<div className="my-5 fs-5">
+						<p className="mt-5">
+						I am a passionate <span>web developer</span> with experience in building responsive and dynamic websites. I specialize in creating <span>clean, elegant, and user-friendly solutions</span> for various web development needs.
 						</p>
+						<p className="mt-5">
+						I have a strong background in <span>HTML, CSS, JavaScript,</span> and various web development frameworks. I am committed to staying up-to-date with the latest web technologies and best practices to deliver high-quality solutions that meet my clients' needs.</p></div>
 						{/* Skills section */}
 						<div className="skillsContainer mt-5 pt-5">
 							{/* Marquee component for scrolling skills */}
