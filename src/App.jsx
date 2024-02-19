2662ec3005a540ace85715d27aaed42dfeff307b
AoifeEdX <ayohwife@gmail.com>
2024-02-19 19:53:41 +0000

    Added Route paths for Home, About, Projects and Contact to function App()

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,10 @@ function App() {
       <Router> {/* Router component for handling navigation */}
         <div>
           <Routes> {/* Routes component for defining different routes */}
-       
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
       </Router>
