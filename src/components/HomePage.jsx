6d291cdb8f0556693e1f3252bc50d87b25b00b35
AoifeEdX <ayohwife@gmail.com>
2024-02-19 22:10:15 +0000

    Wrote brand statement

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -35,13 +35,14 @@ function HomePage() {
 				<Row className="my-5 py-5">
 					<Col md={12}>
 						<h2 className="text-center">Welcome to my <span>portfolio</span></h2>
+						<div className="mx-5 px-5 fs-5">
 						<hr></hr>
-						<p className="my-5 fs-5">
-							<span>Lorem ipsum</span> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						<p className="mt-5">
+						I am a passionate <span>web developer</span> with experience in building responsive and dynamic websites. I specialize in creating <span>clean, elegant, and user-friendly solutions</span> for various web development needs.
 						</p>
-						<p className="my-5 fs-5">
-						<span>Sed ut perspiciatis</span> unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
-						</p>
+						<p className="mt-5">
+						I have a strong background in <span>HTML, CSS, JavaScript,</span> and various web development frameworks. I am committed to staying up-to-date with the latest web technologies and best practices to deliver high-quality solutions that meet my clients' needs.
+						</p></div>
 					</Col>
 				</Row>
 			</Container>
